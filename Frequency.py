58b760c141d117a5e93b95eb6a3c15469887ffa7
Austin Beard <37314248+austinbeard@users.noreply.github.com>
2018-03-15 09:04:23 -0400

    Improvements to hard code
    Less hard coded and fixed frequency range to have +1 to max

--- Frequency.py
+++ Frequency.py
@@ -1,13 +1,23 @@
 import random
 l = []
 lFreq = []
-for num in range(1000):
-  x = random.randint(1, 1000)
+MIN = int(input("What is the smallest number you want in the range: "))
+MAX = int(input("What is the highest number you want in the range: "))
+tries = int(input("How many numbers would you like generated: "))
+freqnum = int(input("What is the smallest number you want to see in the frequency: "))
+
+for num in range(tries):
+  x = random.randint(MIN, MAX)
   l.append(x)
-for y in range(min(l), max(l)):
+
+for y in range(min(l), max(l)+1):
   freq = l.count(y)
-  if freq > 4:
+  if freq >= freqnum:
     pair = ("{}: {} times".format(y, freq))
     lFreq.append(pair)
-for amount in range(len(lFreq)):
-  print(lFreq.pop())
+if not lFreq:
+  print("Noting occured {} or more times!".format(freqnum))
+
+if lFreq:
+  for amount in range(len(lFreq)):
+    print(lFreq.pop())
