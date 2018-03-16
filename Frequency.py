6e1357ac977658d2a1b477392e01cb1ca9d854e4
Austin Beard <37314248+austinbeard@users.noreply.github.com>
2018-03-16 08:08:22 -0400

    Even more spaces in code

--- Frequency.py
+++ Frequency.py
@@ -1,10 +1,11 @@
-import random
+from random import randint
 
 #Initialize both lists used in program
 l = []
 lFreq = []
 
 print("This program generates a list of numbers then gives you the highest or lowest frequency of a number based on your inputs")
+
 #Get user inputted values
 MIN = int(input("What is the smallest number you want in the range: "))
 MAX = int(input("What is the highest number you want in the range: "))
@@ -13,21 +14,26 @@ freqnum = int(input("What is the smallest number you want to see in the frequenc
 
 #Loop to generate list of random numbers
 for num in range(tries):
-  x = random.randint(MIN, MAX)
+  x = randint(MIN, MAX)
+  
   #Put each number into the list
   l.append(x)
 
 #Loop to get the frequency of each number in the first list
 for y in range(min(l), max(l)+1):
   freq = l.count(y)
+  
   #Only count it if the frequency is higher than what the user wants
   if freq >= freqnum:
+    
     #Organize it nicely 
     pair = ("{}: {} times".format(y, freq))
+    
     #Put each number with frequency in list
     lFreq.append(pair)
 
-print("These are the numbers that generated:")
+print("\n\n\nThese are the numbers that generated:")
+
 #Tell the user if the list is empty
 if not lFreq:
   print("Nothing occured {} or more times!".format(freqnum))
@@ -35,5 +41,6 @@ if not lFreq:
 #Otherwise give the user the list they want 
 else:
   for amount in range(len(lFreq)):
+    
     #Prints it not as a list because it's better to read
     print(lFreq.pop())
