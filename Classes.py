bd8abda5fc0241939c9ed2f96c4431252cbbe8db
Austin <37314248+dev-austin@users.noreply.github.com>
2018-04-25 10:08:32 -0400

    Update Classes.py
    Added to conditions

--- Classes.py
+++ Classes.py
@@ -1,31 +1,41 @@
-from random import choice, randint
+from random import randint
 from time import sleep
 
 class Coin:
-  sides = ['heads', 'tails']
   
   def __init__(self, name):
     self.name = name
+    self.sideUp = 'Heads'
     
   def flip(self):
-    side = choice(Coin.sides)
-    return side
+    if randint(0, 1) == 0:
+      self.sideUp = 'Heads'
+    else:
+      self.sideUp = 'Tails'
   
   def displayInfo(self):
-    print("{} flipped {}".format(self.name, self.flip()))
+    self.flip()
+    print("{} flipped {}".format(self.name, self.getSide()))
+  
+  def getSide(self):
+    return self.sideUp
     
     
 class Die:
   
   def __init__(self, numSides):
     self.numSides = numSides
+    self.sideUp = 1
     
   def roll(self):
-    side = randint(1, self.numSides)
-    return side
+    self.sideUp = randint(1, self.numSides)
     
   def displayInfo(self):
-    print("D{} rolled a {}".format(self.numSides, self.roll()))
+    self.roll()
+    print("D{} rolled a {}".format(self.numSides, self.getSide()))
+  
+  def getSide(self):
+    return self.sideUp
     
 dime = Coin('Dime')
 penny = Coin('Penny')
@@ -43,4 +53,12 @@ for x in range(10):
   d6.displayInfo()
   d20.displayInfo()
   print("\n")
+  if dime.sideUp == 'Heads' and penny.sideUp == 'Heads' and quarter.sideUp == 'Heads' and nickel.sideUp == 'Heads':
+    print("All heads!")
+  elif dime.sideUp == 'Tails' and penny.sideUp == 'Tails' and quarter.sideUp == 'Tails' and nickel.sideUp == 'Tails':
+    print("All tails!")
+  elif dime.sideUp == 'Heads' and penny.sideUp == 'Heads' and quarter.sideUp == 'Heads' and nickel.sideUp == 'Heads' and d6.getSide == 6 and d20.getSide == 20:
+    print("All heads and max numbers on dice!")
+  elif dime.sideUp == 'Tails' and penny.sideUp == 'Tails' and quarter.sideUp == 'Tails' and nickel.sideUp == 'Tails' and d6.getSide == 1 and d20.getSide == 1:
+    print("All tails and minimum numbers on dice!")
   sleep(2)
