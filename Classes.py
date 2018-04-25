477d0e59515184509fd9bbdb17dca6ade7f55ee1
Austin <37314248+dev-austin@users.noreply.github.com>
2018-04-25 09:09:52 -0400

    Update and rename Coin.py to Classes.py

--- Classes.py
+++ Classes.py
@@ -14,15 +14,33 @@ class Coin:
   def displayInfo(self):
     print("{} flipped {}".format(self.name, self.flip()))
     
-dime = Coin('dime')
-penny = Coin('penny')
-quarter = Coin('quarter')
-nickel = Coin('nickel')
+    
+class Die:
+  
+  def __init__(self, numSides):
+    self.numSides = numSides
+    
+  def roll(self):
+    side = random.randint(1, self.numSides)
+    return side
+    
+  def displayInfo(self):
+    print("D{} rolled a {}".format(self.numSides, self.roll()))
+    
+dime = Coin('Dime')
+penny = Coin('Penny')
+quarter = Coin('Quarter')
+nickel = Coin('Nickel')
+
+d6 = Die(6)
+d20 = Die(20)
 
 for x in range(10):
   dime.displayInfo()
   penny.displayInfo()
   quarter.displayInfo()
   nickel.displayInfo()
+  d6.displayInfo()
+  d20.displayInfo()
   print("\n")
   time.sleep(2)
